d3fdf09a590c5e779e6a3009283d38fc01dd781b
vladislav-1507 <vlad150789@mail.ru>
2025-10-23 17:06:19 +0300

    исправление замечания to_be_visible()

--- playwright_registration.py
+++ playwright_registration.py
@@ -27,6 +27,5 @@ with sync_playwright() as playwright:
 
     dashboard_toolbar = page.get_by_test_id("dashboard-toolbar-title-text")
 
-    expect(dashboard_toolbar).to_be_visible
+    expect(dashboard_toolbar).to_be_visible()
     expect(dashboard_toolbar).to_have_text("Dashboard")
-    
