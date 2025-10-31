1916b250c6a6cf4c7061023ad3b8ae74d3473dcb
vladislav-1507 <vlad150789@mail.ru>
2025-10-31 16:41:47 +0300

    Pytest skip, skipif, xfail

---
+++ tests/test_pytest_skipif.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+
+
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="Требуется Python 3.8 или выше")
+def test_python_version():
+    pass
+
+SYSTEM_VERSION = "v1.2.0"  # Для примера укажем версию тестируемой системы
+
+
+@pytest.mark.skipif(
+    SYSTEM_VERSION == "v1.3.0",  # Пропустим автотест, если версия системы равна v1.3.0
+    reason="Тест не может быть запущен на версии системы v1.3.0"
+)
+def test_system_version_valid():  # В текущей конфигурации этот тест запустится
+    pass
+
+
+@pytest.mark.skipif(
+    SYSTEM_VERSION == "v1.2.0",  # Пропустим автотест, если версия системы равна v1.2.0
+    reason="Тест не может быть запущен на версии системы v1.2.0"
+)
+def test_system_version_invalid():  # Этот автотест не запустится
+    pass
