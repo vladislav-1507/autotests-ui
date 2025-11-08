9c395e281743a083aee0f9455fe32e76157916bd
vladislav-1507 <vlad150789@mail.ru>
2025-11-08 18:22:56 +0300

    Фикстуры Pytest. Исправление замечаний

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,8 +38,9 @@ def initialize_browser_state(playwright: Playwright) -> None:
     context.storage_state(path="browser-state.json")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()
+    browser.close()
