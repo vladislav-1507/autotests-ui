bc5436e40aab1dff072458baf5160a57043baa26
vladislav-1507 <vlad150789@mail.ru>
2026-02-03 16:02:13 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 
+from components.navigation.sidebar_component import SidebarComponent
 from components.navigation.navbar_component import NavbarComponent
 from pages.base_page import BasePage
 
@@ -7,7 +8,9 @@ from pages.base_page import BasePage
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+        
 
+        self.sidebar = SidebarComponent(page)
         self.navbar = NavbarComponent(page)
 
         self.dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
